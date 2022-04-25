2096efa4c130be331701927c027907cefd72f83a
karolkruczek <66781307+karolkruczek@users.noreply.github.com>
2022-04-26 00:23:46 +0200

    second

--- ssg/site.py
+++ ssg/site.py
@@ -3,16 +3,17 @@ from pathlib import Path
 
 class Site:
     def __init__(self, source, dest):
-        self.source = Path()
-        self.dest = Path()
+        self.source = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory  = self.dest + "/" + self.source.relative_to()
-        mkdir(directory,parents = True, exist_ok = True)
+        directory  = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
+        self.dest.mkdir(parents = True, exist_ok = True)
         for path in self.source.rglob("*"):
-            if path == self.directory:
+            if path.is_dir():
                 self.create_dir(path)
 
 
